100593928d71d1f5baaa6eb3cd4f9dd80209b363
nightmono <163682003+nightmono@users.noreply.github.com>
2024-04-10 13:26:07 +0100

    Add file handling functions

--- shrimple.py
+++ shrimple.py
@@ -2,7 +2,64 @@
 
 import argparse
 from datetime import datetime
+import shlex
 
+def get_reminders_from_file(file=None):
+    if file is None:
+        file = "shrimple-reminders.txt"
+    
+    reminders = []
+    
+    try:
+        with open(file, "r", encoding="utf-8") as f:
+            for line in f.readlines():
+                reminders.append(line)
+    except FileNotFoundError:
+        # Create shrimple-reminders text file if it file not found.
+        with open(file, "w") as f:
+            pass
+            
+    return reminders
+
+def save_reminders_to_file(file=None, reminders=None):
+    if file is None:
+        file = "shrimple-reminders.txt"
+    if reminders is None:
+        reminders = []
+        
+    with open(file, "w", encoding="utf-8") as f:
+        for reminder in reminders:
+            f.write("[X]" if reminder["complete"] else "[-] ")
+            f.write(reminder["reminder"])
+            # := Assigns date to date within the if statement.
+            if (date := reminder.get("date", False)):
+                f.write(f" {date}\n")
+            else:
+                f.write("\n")
+
+def format_reminders(reminders):
+    formatted_reminders = []
+    
+    for reminder in reminders:
+        formatted_reminder = {}
+        # Split via quotation marks. 
+        # The reminder should ideally be in this format (date is optional):
+        # [-] "Reminder content" DD-MM-YYYY
+        # [X] "Reminder content" DD-MM-YYYY
+        # Resulting in these args: [complete_or_not, content, optional_date]
+        reminder_args = shlex.split(reminder)
+        
+        formatted_reminder["complete"] = reminder_args[0] == "[X]"
+        formatted_reminder["reminder"] = reminder_args[1]
+        
+        # Only add the date if it is present.
+        if len(reminder_args) == 3:
+            formatted_reminder["date"] = reminder_args[2]
+        
+        formatted_reminders.append(formatted_reminder)
+    
+    return formatted_reminders
+            
 def date(date):
     # Give support for dd/mm/yyyy, yyyy/mm/dd, mm/dd/yyyy.
     # Support for dd/mm and mm/dd dates too, via appending the current year.
@@ -42,12 +99,6 @@ list_parser.add_argument("--after",
                          help="searches for reminders set to after the provided date")
 list_parser.add_argument("--before", 
                          help="searches for reminders set to before the provided date")
-list_parser.add_argument("--made-on", 
-                         help="searches for reminders created on provided date")
-list_parser.add_argument("--made-after", 
-                         help="searches for reminders created after provided date")
-list_parser.add_argument("--made-before",
-                         help="searches for reminders created before provided date")
 
 complete_parser = subparsers.add_parser("complete",
                                         help="Marks a reminder as complete")
@@ -64,3 +115,8 @@ delete_parser.add_argument("--index",
                              help="delete reminder at passed index")
 
 args = parser.parse_args()
+print(args)
+
+# Print help if ran without arguments.
+if not vars(args):
+    parser.print_help()
