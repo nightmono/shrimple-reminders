2c448973b80de1db22de0dedf26e945334a493f6
nightmono <163682003+nightmono@users.noreply.github.com>
2024-04-10 12:53:17 +0100

    Add all parsers

--- shrimple.py
+++ shrimple.py
@@ -20,13 +20,47 @@ def date(date):
     raise ValueError(f"{date} is an invalid date")
 
 parser = argparse.ArgumentParser(description='A Shrimple reminders app.')
-# Default behaviour is to add a reminder.
-parser.add_argument('reminder', nargs='*',
-                    help='The reminder to add')
-parser.add_argument("--date", required=False, type=date,
-                    help="The date of the reminder")
+subparsers = parser.add_subparsers()
 
-reminder = parser.parse_args().reminder
-reminder = " ".join(reminder)
-print(f"Reminder: {reminder}")
-print("args:", parser.parse_args())
+add_parser = subparsers.add_parser("add", help="Add a new reminder")
+add_parser.add_argument('reminder', nargs='*',
+                    help='reminder description')
+add_parser.add_argument("--date", type=date,
+                        help="date of the reminder")
+
+subparsers.add_parser("today", help="View today's reminders")
+
+list_parser = subparsers.add_parser("list", 
+                                    help="Searches for and lists reminders")
+list_parser.add_argument("--reminder",
+                         help="searches for reminders containing the passed text")
+list_parser.add_argument("-complete", 
+                         help="searches for reminders marked as complete")
+list_parser.add_argument("--on", 
+                         help="searches for reminders set to provided date")
+list_parser.add_argument("--after", 
+                         help="searches for reminders set to after the provided date")
+list_parser.add_argument("--before", 
+                         help="searches for reminders set to before the provided date")
+list_parser.add_argument("--made-on", 
+                         help="searches for reminders created on provided date")
+list_parser.add_argument("--made-after", 
+                         help="searches for reminders created after provided date")
+list_parser.add_argument("--made-before",
+                         help="searches for reminders created before provided date")
+
+complete_parser = subparsers.add_parser("complete",
+                                        help="Marks a reminder as complete")
+complete_parser.add_argument("--reminder",
+                             help="complete reminder that exactly matches passed text")
+complete_parser.add_argument("--index",
+                             help="complete reminder at passed index")
+
+delete_parser = subparsers.add_parser("delete",
+                                      help="Deletes reminders")
+delete_parser.add_argument("--reminder",
+                             help="delete reminder that exactly matches passed text")
+delete_parser.add_argument("--index",
+                             help="delete reminder at passed index")
+
+args = parser.parse_args()
